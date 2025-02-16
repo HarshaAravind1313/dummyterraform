2fca1ba494b6309af7b69adff9ae9aefc896e039
HarshaAravind1313 <149168142+HarshaAravind1313@users.noreply.github.com>
2025-02-16 13:41:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ region = "us-east-1"
 
 terraform {
   backend "remote" {
-    organization = "raham"
+    organization = "harsha"
 
     workspaces {
       name = "dummyterraform"
@@ -15,14 +15,10 @@ terraform {
 
 resource "aws_instance" "three" {
   count         =  3
-  ami           = "ami-00b8917ae86a424c9"
+  ami           = "ami-04681163a08179f28"
   instance_type = "t2.medium"
 
   tags = {
     Name = "mumbai-server"
   }
-}
-
-resource "aws_s3_bucket" "abcd" {
-bucket = "devopsherahamshaik0099889977hcp"
 }
